eea579c17f59c258c034bc06276c61ee53649937
Hwi Jun KIM <euler.bonjour@gmail.com>
2017-08-03 18:11:41 +0900

    fotgot to add comment of license.

--- lib/data_filter/BaseLocalFilter.php
+++ lib/data_filter/BaseLocalFilter.php
@@ -1,5 +1,8 @@
 <?php
 
+// @Auther Hwi Jun KIM. euler.bonjour@gmail.com
+// See License.txt for license of this code.
+
 // call some local program or another object which has the same interface
 
 class BaseLocalFilter {
