afe759326688866e7ce7937c020666e4051d78d7
Hwi Jun KIM <euler.bonjour@gmail.com>
2018-12-13 16:48:46 +0900

    added skelton of process and facade of them. Implementation will be done later on. Those class express business process for simple situation. Even it is assumed that process wrap model for some situation.

---
+++ lib/process/BaseFacade.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("BaseClass.php");
+
+class BaseFacade extends BaseClass {
+  // Type 1 facade express process chain for simple situation
+  // proess1 => proecss2 => process3 ... => process_n
+  // make custom facade for each situation.
+}
+
+?>
---
+++ lib/process/BaseProcess.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("BaseClass.php");
+
+class BaseProcess extends BaseClass {
+  
+}
+
+?>
