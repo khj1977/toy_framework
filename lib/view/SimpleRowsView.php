22b436bbf7c528806fc002ce7154a8b778366635
Hwi Jun KIM <euler.bonjour@gmail.com>
2019-01-24 15:57:01 +0900

    some minor css is applied for table,

--- lib/view/SimpleRowsView.php
+++ lib/view/SimpleRowsView.php
@@ -27,14 +27,11 @@ class SimpleRowsView extends BaseView {
   public function render() {
     $html = "<table>";
     foreach($this->htmlRows as $htmlRow) {
-      $this->ds->vd("is_hidden:");
-      $this->ds->vd($htmlRow);
-      $this->ds->vd($htmlRow->isHidden());
-      if (!$htmlRow->isHidden()) {
-         // continue;
-         $html = $html . $htmlRow->toHTML() . "</br>";
+      if ($htmlRow->isHidden()) {
+         continue;
       }
-      // $html = $html . $htmlRow->toHTML() . "</br>";
+      
+      $html = $html . $htmlRow->toHTML() . "</br>";
     }
     $html = $html . "</table>";
 
