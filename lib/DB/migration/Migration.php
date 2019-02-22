ab48520eaa0c4a7c157a608969ff3621db71ee1c
Hwi Jun KIM <euler.bonjour@gmail.com>
2019-02-22 15:48:08 +0900

    Developing Migration.php. Commit to move branch.

---
+++ lib/DB/migration/Migration.php
@@ -0,0 +1,18 @@
+<?php
+
+// Format of migration
+// CSV
+// up/down
+// def column, add column, drop column
+
+// up,command,col name,type,structure(space separated)
+// up,add_column,a_col,integer(11),unsigned NOT NULL
+// if up is made, down is automatically, made
+// down,dropcolumn,a_col
+
+// version control of miration
+// record of migrations is retained in a table
+// int/PRI KEY,varchar(128),varchar(128),varchar(128),varchar(256)
+// id, up/down, commmand, col_name, structure
+
+?>
