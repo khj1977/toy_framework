ea91abbe421f6e82b112db86cd6993a0f97512c3
Hwi Jun KIM <euler.bonjour@gmail.com>
2019-12-19 16:28:19 +0900

    implmented DBInputStream but not tested yet.

--- lib/input_stream/DBInputStream.php
+++ lib/input_stream/DBInputStream.php
@@ -1,8 +1,13 @@
 <?php
 
 require_once("lib/input_stream/ABSInputStream.php");
+require_once("lib/TheWorld.php");
+require_once("lib/DB/MyPdo.php");
 
 class DBInputStream extends ABSInputStream {
+
+  protected $statement;
+  protected $row;
 
   public function __construct() {
     parent::__construct();
@@ -10,8 +15,20 @@ class DBInputStream extends ABSInputStream {
     return $this;
   }
 
-  public function next() {
+  public function query($sql) {
+    $this->statement = $slave->query($sql);
 
+    return $this;
+  }
+
+  public function next() {
+    $this->row = $this->statement->fetch();
+
+    return $this;
+  }
+
+  public function getItem() {
+    return $this->row;
   }
 
 }
