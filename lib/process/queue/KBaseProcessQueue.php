4dbb3b39ede672fb5097d679d8ce32493d597a3e
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 13:51:30 +0900

    Base class of process queue which express queuing of process has been added. Possibly it express the state such as waiting post office person for deliverly.

---
+++ lib/process/queue/KBaseProcessQueue.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+class KBaseProcessQueue extends BaseClass }
+
+    public function push($process) {
+        // debug
+        // implement this method.
+        // end of debug
+    }
+
+    public function pop() {
+        // debug
+        // implement this method.
+        // end of debug
+    }
+
+?>
