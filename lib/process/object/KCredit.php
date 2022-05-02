9ae50d92fd7d9731684e03fff0e77d405d308175
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 18:02:42 +0900

    Some useful classes for process have been added.

---
+++ lib/process/object/KCredit.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+class KCredit extends BaseClass {
+
+}
+
+?>
---
+++ lib/process/object/KDebt.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+class KDebt extends BaseClass {
+
+}
+
+?>
