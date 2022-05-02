020fd4682ee0fba31fd809ab8fc1d44d4fdb6d16
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-03 05:47:49 +0900

    process pool class has been added.

---
+++ lib/process/lock/KBaseProcessPool.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+// This class is used to make process pause and retain in some state.
+// Typically, wait shipping from manufacture.
+class KBaseProcessPool extends BaseClass {
+
+}
+
+?>
