943adde987eedf65d04b4e16dd27651fa77fc1f0
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 15:54:42 +0900

    Person class has been added. Instance of this class would be owned by process and each person talk to another person and process is run.

---
+++ lib/process/person/KBasePerson.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+// Sub class would be Member, Manager and so on.
+// Process has person and person talks to another person or process.
+class KBasePerson extends BaseClass {
+
+}
+
+?>
