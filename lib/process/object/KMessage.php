8545537f94b3058c932e133f0f714b61ead12752
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 19:05:59 +0900

    message class has been added.

---
+++ lib/process/object/KMessage.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("lib/process/object/KBaseInformation.php");
+
+// This is a base class to exchange message between department or person.
+class KMessage extends KBaseInformation {
+
+}
+
+?>
