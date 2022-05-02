415bb64e171d87ea4877578021c6a017480f3cce
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 14:13:07 +0900

    Process director which manages processes has been added. It is possibly used to manipulate the behaviour of process not the same way of facade. Close to idea of manager in ordinary company.

---
+++ lib/process/director/KBaseProcessDirector.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once("lib/BaseClass.php");
+
+    class KBaseProcessDirector extends BaseClass {
+        public function manage($process) {
+            // debug
+            // implement this method or possibly make abstract.
+            // end of debug
+        }
+    }
+
+?>
