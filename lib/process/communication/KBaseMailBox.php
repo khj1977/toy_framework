a0f6188ff5a487648884746d238b8f510e8eabce
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 16:24:41 +0900

    Some useful classes for biz process have been added.

---
+++ lib/process/communication/KBaseMailBox.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+// This class is used to communicate with each other such as person to person.
+// It is close to an idea of biz process, but slightly different.
+class KBaseMailBox extends BaseClass {
+
+}
+
+?>
---
+++ lib/process/object/KBaseInformation.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+// Subclass of this class express information, document, memo, money, etc.
+class KBaseInformation extends BaseClass {
+
+}
+
+?>
