d9c555b4c2e7ed2aa4c7ec0232de31c48fe3d5a8
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 15:41:16 +0900

    A class to inspect live object has been added. Actually this class is inspired by Smalltalk. Author think that because of Smalltalk, modern computing environment exists. The question, however, is why we cannot talk with application as object as squeak? Is it possible to resolve? In Unix, everything is file. That's fine. If it were web, there is REST fook. Something better could be done.

---
+++ lib/util/KInspector.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+class KInspector extends BaseClass {
+    
+    public function setObject($object) {
+
+    }
+
+    public function talk($f) {
+
+    }
+}
+
+?>
