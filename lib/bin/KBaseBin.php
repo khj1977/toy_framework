5b14e069c1c96e4ca2b705f97c2b3987fd425418
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-03 06:26:20 +0900

    base class of bin which is used to communicate with web app via linux or mac has been implemented.

---
+++ lib/bin/KBaseBin.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+// Talk with web app via batch file on mac or linux.
+class KBaseBin extends BaseClass {
+
+    public function talk($receiver, $json) {
+
+    }
+
+}
+
+?>
