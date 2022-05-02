3f5fd584c0ddc0e623948c2643d7b7154a7e4cf6
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 17:45:38 +0900

    A state class has been added. Depends on state, some information is expressed or even something is triggered.

---
+++ lib/process/state/KBaseObjectState.php
@@ -0,0 +1,15 @@
+<?php
+
+<?php
+
+require_once("lib/BaseClass.php");
+
+// This class expresses state of something such as purchased book.
+// state could be ordered, searching in warehouse or even shipped.
+class KBaseObjectState extends BaseClass {
+
+}
+
+?>
+
+?>
