9854476451c38959aec23ac464a2efef17d0cd17
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-03 06:46:39 +0900

    minor update of base bin.

--- lib/bin/KBaseBin.php
+++ lib/bin/KBaseBin.php
@@ -3,9 +3,10 @@
 require_once("lib/BaseClass.php");
 
 // Talk with web app via batch file on mac or linux.
+// This class is actually inspired by Smalltalk.
 class KBaseBin extends BaseClass {
 
-    public function talk($receiver, $json) {
+    public function talk($receiver, $method, $json) {
 
     }
 
