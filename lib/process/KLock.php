b0b5a7c68fef23d0e228fd10df657629b544317f
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 13:43:26 +0900

    Skelton of visitor for process, node and edge has been added.

---
+++ lib/process/KLock.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+class KLock extends BaseClass {
+
+    public function getLock($state) {
+        // debug
+        // implement this method.
+        // end of debug
+    }
+
+    public function freeLock($state) {
+        // debug
+        // implement this method.
+        // end of debug
+    }
+
+    public function isFree($state) {
+        // debug
+        // implement this method.
+        // end of debug
+    }
+
+}
+
+?>
