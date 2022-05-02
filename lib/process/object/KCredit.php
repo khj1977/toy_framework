be51bbab2abe6953bb65b3e3e29f3be74800c27b
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-02 18:16:50 +0900

    Comment for Japanese translation have been added.

--- lib/process/object/KCredit.php
+++ lib/process/object/KCredit.php
@@ -2,6 +2,7 @@
 
 require_once("lib/process/object/KBaseInformation.php");
 
+// urikake in Japanese
 class KCredit extends KBaseInformation {
 
 }
--- lib/process/object/KDebt.php
+++ lib/process/object/KDebt.php
@@ -2,6 +2,7 @@
 
 require_once("lib/process/object/KBaseInformation.php");
 
+// kaikake in Japanese
 class KDebt extends KBaseInformation {
 
 }
