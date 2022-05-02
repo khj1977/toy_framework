870a533db654e89d4a5c014748dd2b1b933696d9
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-05-03 05:43:55 +0900

    base notification class which is used to notify something to member or some other person has been implemented.

---
+++ lib/process/object/KBaseNotification.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("lib/process/object/KBaseInformation.php");
+
+// This class used as a following case: Suppose customer buy something on EC,
+// then, notification goes to operation dept or person.
+// Notification is kind of information. But also, notification could possible have
+// information.
+class KBaseNotification extends KBaseInformation {
+
+}
+
+?>
