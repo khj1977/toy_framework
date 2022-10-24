e916f0c9f13b83d6c54a4863e70401ebffab7134
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-10-25 04:15:56 +0900

    lock is under development.

--- lib/inspector/KBaseInspector.php
+++ lib/inspector/KBaseInspector.php
@@ -4,12 +4,24 @@ require_once("lib/BaseClass.php");
 
 class KBaseInspector extends BaseClass {
     
-    public function setObject($object) {
+    protected $internalObject;
 
+    protected function initialize() {
+        parent::initialize();
+
+        $this->internalObject = null;
+
+        return $this;
     }
 
-    public function talk($f) {
+    public function setObject($object) {
+        $this->internalObject = $object;
+    }
 
+    // $f could be function object but it is supposed that anon function is
+    // better choice.
+    public function talk($f) {
+        return $f($this->internalObject);
     }
 }
 
--- lib/process/lock/KBaseLock.php
+++ lib/process/lock/KBaseLock.php
@@ -1,30 +1,52 @@
 <?php
 
 require_once("lib/BaseClass.php");
+require_once("lib/process/state/KBaseObjectState.php");
 
+// Lock flow between proccess to process.
+// State of exec chain of process flows.
+// If process is required to be synced with multiple processes,
+// a process should wait another process.
+// In that time, lock is used to lock state of process.
 class KBaseLock extends BaseClass {
+
+    // KBaseProcessState
+    protected $state;
 
     public function initialize()
     {
         parent::initialize();
+
+        $this->state = new KBaseObjectState();
     }
 
-    public function getLock($state) {
+    public function lock($process) {
+        return $this->getLock($process);
+    }
+
+    // return state?
+    public function getLock($process) {
         // debug
         // implement this method.
         // end of debug
     }
 
-    public function freeLock($state) {
+    public function freeLock() {
         // debug
         // implement this method.
         // end of debug
     }
 
-    public function isFree($state) {
+    public function isLocked() {
+        return $this->state->getState();
+    }
+
+    public function isFree() {
         // debug
         // implement this method.
         // end of debug
+
+        return !$this->state->getState();
     }
 
 }
