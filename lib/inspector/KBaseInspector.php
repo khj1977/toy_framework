29d56408f813c85df70dc5b1b776dba9d27f22e4
Hwi Jun KIM <euler.bonjour@gmail.com>
2022-11-08 04:02:29 +0900

    default functions for inspector has been added.

--- lib/inspector/KBaseInspector.php
+++ lib/inspector/KBaseInspector.php
@@ -6,10 +6,18 @@ class KBaseInspector extends BaseClass {
     
     protected $internalObject;
 
+    protected $defaultTalkFunction;
+    protected $defaultPreTalkFunction;
+    protected $defaultPostTalkFunction;
+
     protected function initialize() {
         parent::initialize();
 
         $this->internalObject = null;
+
+        $this->defaultTalkFunction = function($obj) {return;};
+        $this->defaultPreTalkFunction = function($obj) {return;};
+        $this->defaultPostTalkFunction = function($obj) {return;};
 
         return $this;
     }
@@ -21,7 +29,11 @@ class KBaseInspector extends BaseClass {
     // $f could be function object but it is supposed that anon function is
     // better choice. There would be some set of template functions passed to 
     // this method.
-    public function talk($f) {
+    public function talk($f = null) {
+        if ($f == null) {
+            $f = $this->defaultTalkFunction;
+        }
+        
         $this->preTalk();
         $result = $f($this->internalObject);
         $this->postTalk();
