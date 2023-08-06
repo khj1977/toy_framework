ab178bddedd6c12229febb292b335cc5a679eecf
Hwi Jun KIM <euler.bonjour@gmail.com>
2023-08-06 11:52:59 +0900

    interface of deliverly line has been added.

--- lib/process/queue/KBaseDeliveryLine.php
+++ lib/process/queue/KBaseDeliveryLine.php
@@ -2,7 +2,34 @@
 
 require_once("lib/BaseClass.php");
 
+// Kind of facade.
 class KBaseDeliveryLine extends KBaseInformation {
+
+    protected $monitor;
+
+    public function initialize() {
+
+    }
+
+    public function exec() {
+
+    }
+
+    public function addItem($item) {
+
+    }
+
+    public function addFactor($deliverlyProcess, $dependencyProcess) {
+
+    }
+
+    public function relateProcess($process, $relatedProcess) {
+
+    }
+
+    public function setMonitor($f) {
+
+    }
 
 }
 
