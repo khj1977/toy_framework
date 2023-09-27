80434eab6283173d13fc123080684ad6c1ed35bc
Hwi Jun KIM <euler.bonjour@gmail.com>
2023-09-28 05:22:27 +0900

    90% of linear chain facade has been made.

--- lib/process/ChainProcessFacade.php
+++ lib/process/ChainProcessFacade.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once("lib/process/BaseFacade.php");
+require_once("lib/data_struct/KArray.php");
+require_once("lib/data_struct/KHash.php");
 
 class ChainProcessFacade extends BaseFacade {
 
@@ -9,22 +11,28 @@ class ChainProcessFacade extends BaseFacade {
   public function __consturct() {
     parent::__construct();
 
-    $this->processes = array();
+    $this->processes = new KArray();
 
     return $this;
   }
 
   // no remove or pop method because no usage cannot be found at this time.
   public function pushProcess($process) {
-    $this->processes[] = $process;
+    $this->processes->push($process);
   }
 
   public function exec() {
-    foreach($this->processes as $process) {
-      $proess->exec();
-    }
-
+    $this->processes->each(function($process) {
+      $process->exec();
+    });
+  
     return $this;
+  }
+
+  public function traverseByVisitor($visitor) {
+    $this->processes->each(function($process) use ($visitor) {
+      $visitor->exec($process, new KHash());
+    });
   }
 
 }
