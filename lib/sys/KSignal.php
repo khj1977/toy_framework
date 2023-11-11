7cd819424a084296c2df455ddd2b1e721b12b74c
Hwi Jun KIM <euler.bonjour@gmail.com>
2023-11-12 04:00:43 +0900

    Adaptor of signal has been added.

---
+++ lib/sys/KSignal.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once("lib/BaseClass.php");
+
+// Typically, this signal, which is impled by UNIX signal, is used for
+// impl of KTimer.
+class KSignal extends BaseClass {
+
+}
+
+?>
