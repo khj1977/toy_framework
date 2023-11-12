a5fd0a4a251b56df7bac80ad71905c5549c6a79a
Hwi Jun KIM <euler.bonjour@gmail.com>
2023-11-13 03:51:03 +0900

    First version of fire class has been made but not yet tested.

--- lib/sys/KSignal.php
+++ lib/sys/KSignal.php
@@ -1,10 +1,36 @@
 <?php
 
 require_once("lib/BaseClass.php");
+require_once("lib/data_struct/KHash.php");
 
 // Typically, this signal, which is impled by UNIX signal, is used for
 // impl of KTimer.
 class KSignal extends BaseClass {
+
+    protected $signalHandler;
+
+    public function intialize() {
+        $this->signalHandler = new KHash();
+
+        return $this;
+    }
+
+    public function addHandler($sigNo, $f) {
+        $this->signalHandler->set($sigNo, $f);
+
+        pcntl_signal($sigNo, "fireSignal");
+    }
+
+    public function fireSignal($sigNo) {
+        $f = $this->signalHandler->get($this->getSigNoHashKey($sigNo));
+        $f();
+
+        return $this;
+    }
+
+    protected function getSigNoHashKey($sigNo) {
+        return "KSignal::" .  $sigNo;
+    }
 
 }
 
