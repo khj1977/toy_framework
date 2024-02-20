cd9ffad9b6b279a8352163c9b604807fd809b663
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-02-20 10:52:56 +0900

    skelton of calendar view class has been modified. Remaining factors are some test, impl.

--- lib/view/KCalenderView.php
+++ lib/view/KCalenderView.php
@@ -6,7 +6,27 @@ require_once("lib/view/BaseView.php");
 // jquery-ui.
 class KCalendarView extends BaseView {
 
+    public function __construct() {
+        parent::__construct();
+    
+        return $this;
+      }
+    
+      protected function initialize() {
+        parent::initialize();
+    
+        return $this;
+      }
+
     public function render() {
+        // debug
+        // impl render with jquery-ui calendar. Reading js src and css src may not 
+        // be possible with view. Hence it would go to template of view.
+        // Sine they are general lib, it is OK to handle so.
+        // $html = sprintf('<div class="p-4 mb-2 bg-dark text-white">%s</div>', $this->title);
+        // 
+        // return $html;
+        // end of debug
 
     }
 
