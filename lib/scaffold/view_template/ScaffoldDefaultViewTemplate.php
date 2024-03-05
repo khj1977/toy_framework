b1cae4b594360143796eba3f8f465ce014a2e30a
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-03-05 15:56:38 +0900

    css has been changed for date picker.

--- lib/scaffold/view_template/ScaffoldDefaultViewTemplate.php
+++ lib/scaffold/view_template/ScaffoldDefaultViewTemplate.php
@@ -4,7 +4,8 @@
 <meta charset="UTF-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="/css/style.css"> 
 
 <link href="/jquery-ui/jquery-ui.css" rel="stylesheet">
 
