27aa588c6deb3cc3024a6c1742ebe0ed2f498398
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-07-11 15:32:42 +0900

    Auth related class has been added.

---
+++ lib/util/Auth.php
@@ -0,0 +1,29 @@
+<?php
+
+// debug
+// implement this class.
+// end of debug
+
+require_once("lib/BaseClass.php");
+
+class Auth extends BaseClass {
+
+    public function isAuthed() {
+
+    }
+
+    // debug
+    // Don't take log on MyPDO.
+    // end of debug
+    public function askAuth($id, $pass) {
+
+    }
+
+    public function doExpire() {
+
+    }
+
+}
+
+
+?>
