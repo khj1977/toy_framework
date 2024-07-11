464c226707966ca5bc6c1b64f44275dae4a823d9
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-07-11 16:49:56 +0900

    Auth has been impled. Not tested yet.

--- lib/util/Auth.php
+++ lib/util/Auth.php
@@ -5,26 +5,64 @@
 // end of debug
 
 require_once("lib/BaseClass.php");
+require_once("lib/TheWorld.php");
 
 class Auth extends BaseClass {
 
-    public function isAuthed() {
+    protected $isAuthed;
 
+    protected $userName;
+    protected $hashedPassword;
+
+    protected function initialize() {
+        parent::initialize();
+
+        $this->isAuthed = false;
+
+        return $this;
+    }
+
+    public function setUserID($id, $rawPassword) {
+        $this->userName = $id;
+        $this->hashedPassword = $this->getHash($rawPassword);
+
+        return $this;
+    }
+
+    public function isAuthed() {
+        return $this->isAuthed;
     }
 
     // debug
     // Don't take log on MyPDO.
     // end of debug
-    public function askAuth($id, $pass) {
+    public function askAuth() {
+        $sql = "SELECT count(id) as cnt FROM auth WHERE user_name = ? and hashed_password = ?";
 
+        $pdo = TheWorld::instance()->slave;
+        $statement = $pdo->prepare($sql);
+
+        $statement->execute(array($this->userName, $this->hashedPassword));
+        $rows = $statement->fetch();
+
+        $cnt = $rows["cnt"];
+        if ($cnt != "1") {
+            return false;
+        }
+
+        return true;
     }
 
     public function doExpire() {
+        $this->isAuthed = false;
 
+        return $this;
     }
 
     protected function getHash($rawPassword) {
+        $hash = hash("sha256", $rawPassword);
 
+        return $hash;
     }
 
 }
