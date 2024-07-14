908f5f233272288f6a73f8947ebe415f19760c12
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-07-14 21:10:30 +0900

    BaseAuthController has been started to impl.

--- lib/BaseAuthController.php
+++ lib/BaseAuthController.php
@@ -4,12 +4,23 @@
 // See License.txt for license of this code.
 
 require_once("lib/BaseController.php");
+require_once("lib/util/KAuth.php");
 
 // debug
 // implement this class
 // end of debug
 
 class BaseAuthController extends BaseController {
+
+    public function preAction() {
+        $auth = new KAuth();
+
+        if (!$auth->isAuthed()) {
+            // debug
+            // call header location or other non authed handling.
+            // end of debug
+        }
+    }
 
 }
 
