1d0d15d5e273867d709c3eb5bd12423bcf47deef
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-07-15 14:50:59 +0900

    pre/post do has been defined in logic.

--- lib/Logic.php
+++ lib/Logic.php
@@ -8,7 +8,15 @@ require_once("lib/BaseClass.php");
 // This class enclose a logic basically within action of controlller. Since it will be injected to outside of controller, impl of controller or MVC would be more flexible.
 abstract class Logic extends BaseClass {
 
-    abstract public function do();
+    abstract public function xdo();
+
+    public function do() {
+        $this->preDo();
+        $this->xdo();
+        $this->postDo();
+
+        return $this;
+    }
 
     protected function preDo() {
 
