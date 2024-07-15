a1d694143b72ac7d9c84293866027bf8832c37ad
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-07-15 14:49:37 +0900

    minor fix.

--- lib/Logic.php
+++ lib/Logic.php
@@ -5,7 +5,7 @@
 
 require_once("lib/BaseClass.php");
 
-// This class enclose a logic basically within action of controlller. Since it is injected to outside of controller, impl of controller or MVC would be more flexible.
+// This class enclose a logic basically within action of controlller. Since it will be injected to outside of controller, impl of controller or MVC would be more flexible.
 abstract class Logic extends BaseClass {
 
     abstract public function do();
