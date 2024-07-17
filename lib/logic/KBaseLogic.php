9181a5256f43ef75545e674ff5c34760a893fadc
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-07-17 18:35:44 +0900

    BaseLogic has been renamed to KBaseLogic which is actually git mv.

--- lib/logic/KBaseLogic.php
+++ lib/logic/KBaseLogic.php
@@ -1,7 +1,31 @@
 <?php
 
-// debug
-// implement this class
-// end of debug
+// @Auther Hwi Jun KIM. euler.bonjour@gmail.com
+// See License.txt for license of this code.
+
+require_once("lib/BaseClass.php");
+
+// This class enclose a logic basically within action of controlller. Since it will be injected to outside of controller, impl of controller or MVC would be more flexible.
+abstract class KBaseLogic extends BaseClass {
+
+    abstract protected function xdo();
+
+    final public function do() {
+        $this->preDo();
+        $this->xdo();
+        $this->postDo();
+
+        return $this;
+    }
+
+    protected function preDo() {
+
+    }
+
+    protected function postDo() {
+
+    }
+
+}
 
 ?>
