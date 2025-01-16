4bf3317fda7f76912dcbf1a97a30fc9f1815c84d
Hwi Jun KIM <euler.bonjour@gmail.com>
2025-01-17 07:46:09 +0900

    Helper class for string has been added. for alternative of KString::cmd() or something like that, it would be used.

---
+++ lib/util/KS.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once("lib/data_struct/KString.php");
+
+class KS extends KString {
+
+    static public function n() {
+
+    }
+
+    static public function make() {
+
+    }
+
+    static public function cmp() {
+
+    }
+
+    static public function eq() {
+
+    }
+
+    static public function copy() {
+        
+    }
+
+}
+
+?>
